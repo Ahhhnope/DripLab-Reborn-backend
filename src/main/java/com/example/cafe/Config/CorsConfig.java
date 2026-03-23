0b682e23bafcd118009ee02b54eba84eed13e81f
Ahhhnope <magelova32@gmail.com>
2026-03-23 11:27:58 +0700

    added localhost:5005 and 5173 to cors config

--- src/main/java/com/example/cafe/Config/CorsConfig.java
+++ src/main/java/com/example/cafe/Config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5173")
+                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5173", "http://localhost:5005")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
