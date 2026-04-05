69eb59eca6c5059a897bd6352823d24977f3d82f
Ahhhnope <magelova32@gmail.com>
2026-04-05 20:41:58 +0700

    .

--- src/main/java/com/example/cafe/Config/SecurityConfig.java
+++ src/main/java/com/example/cafe/Config/SecurityConfig.java
@@ -1,12 +1,53 @@
 package com.example.cafe.Config;
 
 
+import com.example.cafe.Security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.Arrays;
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->auth
+                        .requestMatchers("/api/auth/**").permitAll()
+
+                        //Things needed for the website without login (menu and promo codes ui stuff)
+                        .requestMatchers(HttpMethod.GET, "/api/promo-codes/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/drinks/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/ingredients/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/toppings/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/sizes/**").permitAll()
+
+                        //after login for users
+                        .requestMatchers(HttpMethod.GET, "/api/carts/**").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/orders/**").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/invoices/**").authenticated()
+
+                        //for if you want to store images in this api
+                        .requestMatchers("/IMG/**").permitAll()
+
+                        //admin can just do everything ;-;
+                        .anyRequest().hasRole("ADMIN")
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
