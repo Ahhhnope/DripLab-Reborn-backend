234b73756240fb291ff5798574f35d819d5cf4a1
marcodma0101 <marcodma0101@gmail.com>
2026-04-05 10:53:32 +0700

    bi loi phan cors kia

--- src/main/java/com/example/cafe/Config/WebConfig.java
+++ src/main/java/com/example/cafe/Config/WebConfig.java
@@ -1,19 +1,15 @@
 package com.example.cafe.Config;
 
 import org.springframework.context.annotation.Configuration;
-<<<<<<< HEAD
 import org.springframework.web.servlet.config.annotation.*;
-=======
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
->>>>>>> b2f124e5dda585e0810bbd1108ad5545aae30863
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-<<<<<<< HEAD
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
@@ -25,12 +21,5 @@ public class WebConfig implements WebMvcConfigurer {
         String uploadPath = "file:" + System.getProperty("user.dir") + "/uploads/";
         registry.addResourceHandler("/IMG/**")
                 .addResourceLocations(uploadPath + "IMG/");
-=======
-        registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000") // Add your Vue URL here
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
->>>>>>> b2f124e5dda585e0810bbd1108ad5545aae30863
     }
 }
